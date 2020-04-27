dacde61f03aab7bdba1a57251e87650318618b0f
leosaline <leosaline@gmail.com>
2020-04-27 20:34:08 -0300

    refactor

--- src/main/java/com/saline/naton/company/CompanyController.java
+++ src/main/java/com/saline/naton/company/CompanyController.java
@@ -6,9 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +21,7 @@ public class CompanyController {
 	private CompanyRepository repo;
 	
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Return list of companies") })
-	@RequestMapping(value = "/companies", method = RequestMethod.GET, produces = "application/json")
+	@GetMapping(value = "/companies", produces = "application/json")
 	@CrossOrigin(origins = {"http://localhost:8080", " http://natonfrontend:8080"})
 	@ResponseBody
 	public ResponseEntity<Collection<Company>> listProducts() {
@@ -30,7 +29,7 @@ public class CompanyController {
 	}
 	
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Return Company by ID") })
-	@RequestMapping(value = "/company/{id}", method = RequestMethod.GET, produces = "application/json")
+	@GetMapping(value = "/company/{id}", produces = "application/json")
 	@CrossOrigin(origins = {"http://localhost:8080", " http://natonfrontend:8080"})
 	@ResponseBody
 	public ResponseEntity<Company> companyById(@PathVariable Long id) {
